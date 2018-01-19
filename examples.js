45ac70ff80847f65b60fcf619c9660cf00dd0709
agorischek sterling <alex.gorischek@gmail.com>
2018-01-18 21:46:59 -0800

    Add example usage

---
+++ examples.js
@@ -0,0 +1,23 @@
+// Example uses of Markunit
+
+var markunit = require("markunit")
+var fs = require("fs");
+var readme = markunit(fs.readFileSync("./README.md", "utf8"))
+
+describe("Documentation", function(){
+  it("should have a title", function(){
+    readme.markup.has("h1")
+  })
+  it("should not have any curly quotes in code snippets", function(){
+    readme.code.no(["“","”"])
+  })
+  it("should have installation instructions", function(){
+    readme.code.has("npm install")
+  })
+  it("should capitalize 'HTML' when used in copy", function(){
+    readme.copy.no("html")
+  })
+  it("should not contain double-indented lists", function(){
+    readme.markup.no("li li")
+  })
+})
