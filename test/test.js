f2e6523548e77462e69b91a347610689371718b4
agorischek sterling <alex.gorischek@gmail.com>
2018-01-18 20:10:09 -0800

    Add support for submitting arrays as patterns

--- test/test.js
+++ test/test.js
@@ -13,6 +13,9 @@ describe("Source", function(){
     it("should find a pattern with regex", function(){
         doc.source.has(/fil.+?ed/)
     })
+    it("should find a subpattern from an array", function(){
+        doc.source.has(["Test", /abcd/, "document"])
+    })
     it("should not find a string with different casing", function(){
         doc.source.no("# test document")
     })
@@ -21,6 +24,9 @@ describe("Source", function(){
     })
     it("should not find a pattern that doesn't exist with regex", function(){
         doc.source.no(/filll.+?ed/)
+    })
+    it("should not find a subpattern from an array that doesn't exist", function(){
+        doc.source.no(["Not in there", /abcd/])
     })
 })
 
@@ -34,6 +40,9 @@ describe("Rendered", function(){
     it("should find a pattern with regex", function(){
         doc.rendered.has(/<em>.*?<\/em>/)
     })
+    it("should find a subpattern from an array", function(){
+        doc.source.has(["<p>", "<img>", "<b>"])
+    })
     it("should not find an HTML element with incorrect content", function(){
         doc.rendered.no("<h1>Not the title</h1>")
     })
@@ -42,6 +51,9 @@ describe("Rendered", function(){
     })
     it("should not find a pattern that doesn't exist with regex", function(){
         doc.rendered.no(/^<p>/)
+    })
+    it("should not find a subpattern from an array that doesn't exist", function(){
+        doc.source.no(["<i>", /abcd/])
     })
 })
 
@@ -55,6 +67,9 @@ describe("Copy", function(){
     it("should find a pattern with regex", function(){
         doc.copy.has(/testing .*? of/)
     })
+    it("should find a subpattern from an array", function(){
+        doc.source.has(["Test", /abcd/, "document"])
+    })
     it("should not find a string that only exists in code elements", function(){
         doc.copy.no("markunit.js")
     })
@@ -67,8 +82,11 @@ describe("Copy", function(){
     it("should not find a pattern that doesn't exist with regex", function(){
         doc.copy.no(/of .*? only/)
     })
+    it("should not find a subpattern from an array that doesn't exist", function(){
+        doc.source.no(["Not in there", /1234/])
+    })
 })
-//
+
 describe("Code", function(){
     it("should find a string in an inline code element", function(){
         doc.code.has("markunit.js")
@@ -78,6 +96,9 @@ describe("Code", function(){
     })
     it("should find a pattern with regex", function(){
         doc.code.has(/var.*?true/)
+    })
+    it("should find a subpattern from an array", function(){
+        doc.source.has(["mark", "javascript", "document"])
     })
     it("should not find code that isn't present", function(){
         doc.code.no("$ npm install")
@@ -90,6 +111,9 @@ describe("Code", function(){
     })
     it("should not find a pattern that doesn't exist with regex", function(){
         doc.code.no(/testing ==/)
+    })
+    it("should not find a subpattern from an array that doesn't exist", function(){
+        doc.source.no(["JS", /html/])
     })
 })
 
